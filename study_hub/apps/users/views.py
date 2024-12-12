ebcff7dac9d66b2361ff826f023ee876610d8071
Revy1664 <steinsgate95@mail.ru>
2024-12-12 22:35:57 +0600

    some changes

--- study_hub/apps/users/views.py
+++ study_hub/apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
@@ -28,16 +28,18 @@ def profile(request):
 @login_required
 def edit_profile(request):
 
-	form = EditProfileForm()
+	form = EditProfileForm(instance=request.user.profile)
 
 	if request.method == "POST":
 		form = EditProfileForm(request.POST)
 		if form.is_valid():
 			age = form.cleaned_data.get("age")
 			phone_number = form.cleaned_data.get("phone_number")
+			bio = form.cleaned_data.get("bio")
 
 			request.user.profile.age = age
 			request.user.profile.phone_number = phone_number
+			request.user.profile.bio = bio
 
 			request.user.profile.save()
 			return redirect("profile")
